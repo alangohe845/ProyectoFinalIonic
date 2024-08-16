e191651680dc2718d560a14008c6c78a78dcf078
Angel-Eduardo-Renteria-Meza <ingeduardorenteriamz@gmail.com>
2024-08-15 19:05:56 -0600

    config enviroment success

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,19 +2,35 @@
 // `ng build` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
+// export const environment = {
+//   production: false,
+//    firebaseConfig : {
+//     apiKey: "AIzaSyCUKdlTgeEH_CcVpDZQ387vCKPm2C8qzSs",
+//     authDomain: "auth-test-7b9fb.firebaseapp.com",
+//     projectId: "auth-test-7b9fb",
+//     storageBucket: "auth-test-7b9fb.appspot.com",
+//     messagingSenderId: "909454782709",
+//     appId: "1:909454782709:web:70ca68de07cf04e84fd5bc",
+//     measurementId: "G-8TMKGEFEKD"
+//   }
+// };
+
+
 export const environment = {
   production: false,
-   firebaseConfig : {
-    apiKey: "AIzaSyCUKdlTgeEH_CcVpDZQ387vCKPm2C8qzSs",
-    authDomain: "auth-test-7b9fb.firebaseapp.com",
-    projectId: "auth-test-7b9fb",
-    storageBucket: "auth-test-7b9fb.appspot.com",
-    messagingSenderId: "909454782709",
-    appId: "1:909454782709:web:70ca68de07cf04e84fd5bc",
-    measurementId: "G-8TMKGEFEKD"
+  firebaseConfig: {
+    apiKey: "AIzaSyBul9n_G0G1sztuRvIv1YQ09ilsGJgtZ8A",
+    authDomain: "intento1-3eb8f.firebaseapp.com",
+    projectId: "intento1-3eb8f",
+    storageBucket: "intento1-3eb8f.appspot.com",
+    messagingSenderId: "376907960967",
+    appId: "1:376907960967:web:1fe4599bbb92ce27cc5e22"
   }
 };
 
+
+// Initialize Firebase
+// const app = initializeApp(firebaseConfig);
 /*
  * For easier debugging in development mode, you can import the following file
  * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
